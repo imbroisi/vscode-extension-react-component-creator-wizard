a852efb4a71a66db9fe13bd122c89109423ebfe3
Luiz Imbroisi <luizimbroisi@gmail.com>
2024-08-04 14:47:41 -0300

    code clean up

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,9 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testTimeout: 20000,
-    transform: {
-      transform_regex: ['ts-jest', { tsconfig: './tsconfig.json', }],
-    },
+    // transform: {
+    //   transform_regex: ['ts-jest', { tsconfig: './tsconfig.json', }],
+    // },
     testPathIgnorePatterns: ['<rootDir>/src/__tests-vscode__/'],
     testMatch: ['**/src/**/*.test.ts']
   };
