c6f1cc4cbaf3bc84d50a5365e28ff54906e0ce0d
Luiz Imbroisi <luizimbroisi@gmail.com>
2024-08-05 12:47:04 -0300

    v1.0.6 tmp 4

--- src/extension.ts
+++ src/extension.ts
@@ -1,122 +1,100 @@
 import * as vscode from 'vscode';
 import createAllFiles from './components/main';
 import { appData, ASK_ON_COMPONENT_CREATION } from './data/appData';
-import { validateComponentName } from './utils/validateComponentName';
-import { getComponentName } from './extensionHelpers/getComponentName';
+// import { validateComponentName } from './utils/validateComponentName';
+import { getComponentName } from './extensionFiles/getComponentName';
+import { activateExtensions } from './extensionFiles/activateExtensions';
 
 export interface OptionsSelected {
   [key: string]: string
 }
 
-// const getComponentName = async (uri: vscode.Uri) => {
-//   const name = await vscode.window.showInputBox({
-//     placeHolder: 'Enter the Component name',
-//   });
+// async function XXXactivateExtensions(
+//   context: vscode.ExtensionContext,
+//   type: string,
+// ) {
 
-//   if (!name) {
-//     return { error: 'No file name entered' };
+//   // get the options previous selected, if exists
+//   let previousSelections: any = context.globalState.get<Record<string, OptionsSelected>>('optionsSelected');
+//   if (!previousSelections) {
+//     previousSelections = {};
+//     appData.forEach((data) => {
+//       previousSelections[data.id] = data.options[0];
+//     });
 //   }
 
-//   const FolderPath = `${uri.fsPath}/${name}`;
-//   const isPathExist = await vscode.workspace.fs.stat(vscode.Uri.file(FolderPath)).then(() => true, () => false);
-//   if (isPathExist) {
-//     return { error: 'Directory already exist'};
-//   }
-//   if (!validateComponentName(name)) {
-//     return { error: 'Invalid Component name'};
+//   // prompt the user to select the options
+//   const optionsSelected: any = { ...previousSelections };
+//   for (let data of appData) {
+//     const { id, question, options, include_option_ask_on_component_creation } = data;
+//     const previousSelection: any = previousSelections?.[id];
+
+//     const displayOption = [...options];
+//     if (type === 'setup') {
+//       if (include_option_ask_on_component_creation) {
+//         displayOption.push(ASK_ON_COMPONENT_CREATION);
+//       }
+//     }
+
+//     if (typeof previousSelection === 'string') {
+//       const index = displayOption.indexOf(previousSelection);
+//       if (index > -1) {
+//         displayOption.splice(index, 1);
+//         if (type === 'setup' || include_option_ask_on_component_creation) {
+//           displayOption.unshift(previousSelection);
+//         }
+//       }
+//       if (type === 'create') {
+//         if (previousSelection !== ASK_ON_COMPONENT_CREATION) {
+//           continue;
+//         }
+//       }
+//     }
+
+//     const selectedOption = await vscode.window.showQuickPick(displayOption, {
+//       placeHolder: question,
+//     });
+
+//     if (!selectedOption) {
+//       vscode.window.showInformationMessage('No option selected');
+//       return;
+//     }
+
+//     optionsSelected[id] = selectedOption;
 //   }
 
-//   return { name };
-// };
+//   // save the selected options
+//   if (type === 'setup') {
+//     try {
+//       context.globalState.update('optionsSelected', optionsSelected);
+//     } catch (error) {
+//       vscode.window.showErrorMessage(`Error saving the selected options: ${error}`);
+//       return;
+//     }
+//   }
 
-async function commun(
-  context: vscode.ExtensionContext,
-  uri: vscode.Uri,
-  type: string,
-) {
-  // get the component name if the type is 'create'
-  let componentName: string | undefined = '';
-  if (type === 'create') {
+//   return optionsSelected;
+// }
+
+export function activate(context: vscode.ExtensionContext) {
+  //const create = 
+  vscode.commands.registerCommand('extension.createReactComponentWizard', async (uri: vscode.Uri) => {
+
     const { name, error } = await getComponentName(uri);
     if (error) {
       vscode.window.showInformationMessage(error);
       return;
     }
-    componentName = name;
-  }
+    
+    const optionsSelected = await activateExtensions(context, 'create');
 
-  // get the options previous selected, if exists
-  let previousSelections: any = context.globalState.get<Record<string, OptionsSelected>>('optionsSelected');
-  if (!previousSelections) {
-    previousSelections = {};
-    appData.forEach((data) => {
-      previousSelections[data.id] = data.options[0];
-    });
-  }
+    createAllFiles(uri, name || '', optionsSelected);
 
-  // prompt the user to select the options
-  const optionsSelected: any = { ...previousSelections };
-  for (let data of appData) {
-    const { id, question, options, include_option_ask_on_component_creation } = data;
-    const previousSelection: any = previousSelections?.[id];
-
-    const displayOption = [...options];
-    if (type === 'setup') {
-      if (include_option_ask_on_component_creation) {
-        displayOption.push(ASK_ON_COMPONENT_CREATION);
-      }
-    }
-
-    if (typeof previousSelection === 'string') {
-      const index = displayOption.indexOf(previousSelection);
-      if (index > -1) {
-        displayOption.splice(index, 1);
-        if (type === 'setup' || include_option_ask_on_component_creation) {
-          displayOption.unshift(previousSelection);
-        }
-      }
-      if (type === 'create') {
-        if (previousSelection !== ASK_ON_COMPONENT_CREATION) {
-          continue;
-        }
-      }
-    }
-
-    const selectedOption = await vscode.window.showQuickPick(displayOption, {
-      placeHolder: question,
-    });
-
-    if (!selectedOption) {
-      vscode.window.showInformationMessage('No option selected');
-      return;
-    }
-
-    optionsSelected[id] = selectedOption;
-  }
-
-  // save the selected options
-  if (type === 'setup') {
-    try {
-      context.globalState.update('optionsSelected', optionsSelected);
-    } catch (error) {
-      vscode.window.showErrorMessage(`Error saving the selected options: ${error}`);
-      return;
-    }
-  }
-
-  // create the files if the type is 'create'
-  if (type === 'create') {
-    createAllFiles(uri, componentName || '', optionsSelected);
-  }
-}
-
-export function activate(context: vscode.ExtensionContext) {
-  const create = vscode.commands.registerCommand('extension.createReactComponentWizard', (uri: vscode.Uri) => {
-    commun(context, uri, 'create');
   });
 
-  const setup = vscode.commands.registerCommand('extension.createReactComponentWizardCustomize', (uri: vscode.Uri) => {
-    commun(context, uri, 'setup');
+  // const setup = 
+  vscode.commands.registerCommand('extension.createReactComponentWizardCustomize', (uri: vscode.Uri) => {
+    activateExtensions(context, 'setup');
   });
 
   // context.subscriptions.push(create);
